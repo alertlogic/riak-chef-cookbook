eeb14292e57d111bab3f7ccebf7169a06855ad11
Hector Castro <hectcastro@gmail.com>
2014-02-05 14:49:48 -0500

    Update Riak custom repository installation support for Riak 2.0.

--- recipes/default.rb
+++ recipes/default.rb
@@ -71,7 +71,7 @@ node["riak"]["patches"].each do |patch|
 end
 
 service "riak" do
-  supports :restart => true, :status => true
+  supports :start => true, :stop => true, :restart => true, :status => true
   action [ :enable, :start ]
   not_if { node["riak"]["install_method"] == "source" || node["platform"] == "fedora" }
 end
